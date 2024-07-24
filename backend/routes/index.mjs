388334dc1f133feeeb8afd048219781d68f7e50c
Sebastian Schoeneberger <sebastian.schoeneberger@gmail.com>
2024-07-24 10:12:30 +0200

    Backend Neon Database connection with sequelize and Create Post function

--- backend/routes/index.mjs
+++ backend/routes/index.mjs
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import { createPost } from '../controllers/posts.js';
+
 const router = Router();
 
 router.get('/', (req, res) =>
 {
     res.send('API Home');
 });
+
+router.post('/posts', createPost);
 
 export default router;
